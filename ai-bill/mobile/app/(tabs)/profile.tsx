3e81503bdaad665cd960fe0b85f784b082f2ade0
ssyy <1686584317@qq.com>
2025-09-29 13:56:47 +0800

    登陆注册完成

--- ai-bill/mobile/app/(tabs)/profile.tsx
+++ ai-bill/mobile/app/(tabs)/profile.tsx
@@ -1,23 +1,11 @@
-import { Session } from '@supabase/supabase-js'
-import { useEffect, useState } from 'react'
-import { View } from 'react-native'
-import Account from '../../components/Account'
-import Auth from '../../components/Auth'
-import { supabase } from '../../lib/supabase'
+import { View } from 'react-native';
+import Account from '../../components/Account';
+import Auth from '../../components/Auth';
+import { useAuth } from '../_layout';
 
 export default function App() {
-  const [session, setSession] = useState<Session | null>(null)
-
-  useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session)
-    })
-
-    supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session)
-    })
-  }, [])
-
+  //const [session, setSession] = useState<Session | null>(null)
+  const session = useAuth((state:any) => state.session);
   return (
     <View>
       {session && session.user ? <Account key={session.user.id} session={session} /> : <Auth />}
--- ai-bill/mobile/app/_layout.tsx
+++ ai-bill/mobile/app/_layout.tsx
@@ -1,17 +1,38 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
 import '../global.css';
 
+import { supabase } from '@/lib/supabase';
+import { create } from 'zustand';
+
 export const unstable_settings = {
   anchor: '(tabs)',
 };
 
+export const useAuth = create((set) => ({
+  session:null,
+  setSession:(session:any) => set(session),
+}));
+
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+
+  const setSession = useAuth((state:any) => state.setSession)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession({session})
+    })
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession({session})
+    })
+  }, [])
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
